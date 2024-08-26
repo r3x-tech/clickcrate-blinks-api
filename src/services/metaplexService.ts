81c5b8ba0d4a1d75970ce68dc1997113468344a6
Nabeel77 <nabeel.beeni30@gmail.com>
2024-08-26 22:34:04 +0300

    feature: added function to create collection and nft into the collection

--- src/services/metaplexService.ts
+++ src/services/metaplexService.ts
@@ -1,37 +1,92 @@
-import { Connection, Keypair, PublicKey } from "@solana/web3.js";
-import {
-  Metaplex,
-  keypairIdentity,
-  irysStorage,
-} from "@metaplex-foundation/js";
-import { ProductInfo } from "../schemas/creator";
+import { PublicKey, getBlock } from "@solana/web3.js";
+import { signerIdentity, createNoopSigner } from '@metaplex-foundation/umi'
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { create, createCollection, fetchCollection, mplCore } from "@metaplex-foundation/mpl-core";
+import { irysUploader } from '@metaplex-foundation/umi-uploader-irys'
+import { fromWeb3JsPublicKey } from "@metaplex-foundation/umi-web3js-adapters";
+import { setComputeUnitPrice } from "@metaplex-foundation/mpl-toolbox";
 
-//TODO: Upgrade to create nft collections and nfts using Metaplex Core. Both should include including the attribute plugin and attributes in plugin should match those in JSON perfectly.
-const connection = new Connection(process.env.SOLANA_RPC_URL!);
-const keypair = Keypair.fromSecretKey(
-  Buffer.from(JSON.parse(process.env.WALLET_PRIVATE_KEY!))
-);
-const metaplex = Metaplex.make(connection)
-  .use(keypairIdentity(keypair))
-  .use(irysStorage());
+/**
+ * 
+ * @param collectionName: string 
+ * @param imageUri: string
+ * @param collectionSymbol: string
+ * @param publicKey: PublicKey 
+ * @param network: any (for now) 
+ * @param plugins: array of type any (for now) 
+ */
 
-export async function createMetaplexNFT(productInfo: ProductInfo) {
+export const createMetaplexCollection = async (
+  collectionName: string, 
+  imageUri: string, 
+  collectionSymbol: string, 
+  publicKey: PublicKey, 
+  network: any, 
+  plugins: any[]
+) => {
   try {
-    const { uri } = await metaplex.nfts().uploadMetadata({
-      name: productInfo.name,
-      description: productInfo.description,
-      image: productInfo.imageUri,
+    const umiStandardPublicKey = fromWeb3JsPublicKey(publicKey);
+    const umi = createUmi(network).use(mplCore()).use(irysUploader());
+    const collectionSigner = createNoopSigner(umiStandardPublicKey);
+    umi.use(signerIdentity(collectionSigner)); // if this does not work, can use signerPayer
+    const uri = await umi.uploader.uploadJson({
+      symbol: collectionSymbol,
+      description: 'This is my test NFT collection',
+      image: imageUri,
     });
-
-    const { nft } = await metaplex.nfts().create({
+    const tx = await createCollection(umi, {
+      collection: collectionSigner,
+      name: collectionName,
       uri,
-      name: productInfo.name,
-      sellerFeeBasisPoints: 0,
-    });
-
-    return nft;
+      updateAuthority: umiStandardPublicKey,
+      plugins
+    }).append(setComputeUnitPrice(umi, { microLamports: 1000 }));
+    return tx;
   } catch (error) {
-    console.error("Error creating Metaplex NFT:", error);
+    console.error("Error creating metaplex collection", error);
+    throw error;
+  }
+}
+
+/**
+ * 
+ * @param nftName: string
+ * @param nftImageUri: string 
+ * @param publicKey: publicKey 
+ * @param network: any (for now) 
+ * @param plugins: array of type any (for now) 
+ * @param attributes: array of type any (for now) 
+ */
+
+export const createMetaplexNFTt = async (
+  nftName: string,
+  nftImageUri: string,
+  collectionAddress: any,
+  publicKey: PublicKey, 
+  network: any, 
+  plugins: any[],
+  attributes: any[] = []
+) => {
+  try {
+    const umiStandardPublicKey = fromWeb3JsPublicKey(publicKey);
+    const umi = createUmi(network).use(mplCore()).use(irysUploader());
+    const collection = await fetchCollection(umi, collectionAddress.publicKey);
+    const assetSigner = createNoopSigner(umiStandardPublicKey);
+    const uri = await umi.uploader.uploadJson({
+      description: 'This is my test NFT',
+      image: nftImageUri,
+      attributes
+    });
+    const tx = await create(umi, {
+      asset: assetSigner,
+      collection,
+      name: nftName,
+      uri,
+      plugins
+    }).prepend(setComputeUnitPrice(umi, { microLamports: 1000 }));
+    return tx;
+  } catch(error) {
+    console.error("Error creating metaplex nft", error);
     throw error;
   }
 }
