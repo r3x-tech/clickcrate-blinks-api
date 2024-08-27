f98bee58e74e711cdd492eb4b8ba12c07d80c34d
adrian-petrykiw <cucumber901@gmail.com>
2024-08-27 14:34:34 -0500

    fix route

--- src/routes/creator.ts
+++ src/routes/creator.ts
@@ -28,7 +28,7 @@ router.get("/", (req, res) => {
       links: {
         actions: [
           {
-            href: `/api/creator/create-product`,
+            href: `/creator/create-product`,
             label: "START CREATING",
             parameters: [
               {
