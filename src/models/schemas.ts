3c716086b20c81825c9760ecb7bef1215f741d93
adrian-petrykiw <cucumber901@gmail.com>
2024-09-09 20:10:55 -0500

    adjust

---
+++ src/models/schemas.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+
+export const ProductTypeSchema = z.enum([
+  "Heavyweight Premium T-Shirt (Screen Print)",
+  "Premium Pullover Hoodie (Screen Print)",
+  "Embroidered Dad Hat",
+]);
+
+export const ProductTypes = [
+  {
+    value: "tshirt",
+    label: "Heavyweight Premium T-Shirt (Screen Print)" as const,
+  },
+  {
+    value: "hoodie",
+    label: "Premium Pullover Hoodie (Screen Print)" as const,
+  },
+  {
+    value: "hat",
+    label: "Embroidered Dad Hat" as const,
+  },
+] as const;
+
+export const ProductInfoSchema = z.object({
+  type: ProductTypeSchema,
+  imageUri: z.string().url(),
+  name: z.string().min(1).max(100),
+  description: z.string().max(500),
+  quantity: z.number().int().min(1).max(3),
+  unitPrice: z.number().positive(),
+  email: z.string().email(),
+  account: z.string(),
+});
+
+export const ShippingDetailsSchema = z.object({
+  shippingName: z.string(),
+  shippingEmail: z.string().email(),
+  shippingPhone: z.string().nullable().optional(),
+  shippingAddress: z.string(),
+  shippingCity: z.string(),
+  shippingStateProvince: z.string(),
+  shippingCountryRegion: z.string(),
+  shippingZipCode: z.string(),
+});
+
+export type ProductType = z.infer<typeof ProductTypeSchema>;
+export type ProductInfo = z.infer<typeof ProductInfoSchema>;
+export type ShippingInfo = z.infer<typeof ShippingDetailsSchema>;
---
+++ src/services/solanaService.ts
@@ -0,0 +1,299 @@
+// src/services/solanaService.ts
+import {
+  Cluster,
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  LAMPORTS_PER_SOL,
+  TransactionInstruction,
+  VersionedTransaction,
+  TransactionMessage,
+  ComputeBudgetProgram,
+  Transaction,
+  ParsedTransactionWithMeta,
+  TransactionConfirmationStrategy,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import { ProductInfo } from "../models/schemas";
+import { v4 as uuidv4 } from "uuid";
+import * as MetaplexService from "../services/metaplexService";
+import bs58 from "bs58";
+
+require("dotenv").config();
+
+const rpcUrl =
+  process.env.SOLANA_MAINNET_RPC_URL || "https://api.mainnet.solana.com";
+if (typeof rpcUrl !== "string" || rpcUrl == undefined || !rpcUrl) {
+  throw new TypeError("rpcUrl expected string");
+}
+const connection = new Connection(rpcUrl, {
+  commitment: "confirmed",
+});
+
+if (!process.env.PROXY_WALLET_SK || process.env.PROXY_WALLET_SK == undefined) {
+  throw new TypeError("Proxy wallet secret key not found");
+}
+const relayWalletKeypair = Keypair.fromSecretKey(
+  bs58.decode(process.env.PROXY_WALLET_SK)
+);
+
+async function getTransactionDetails(txSignature: string): Promise<any> {
+  const myHeaders = new Headers();
+  myHeaders.append("x-api-key", process.env.SHYFT_API_KEY!);
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+  };
+
+  const response = await fetch(
+    `https://api.shyft.to/sol/v1/transaction/parsed?network=mainnet-beta&txn_signature=${txSignature}`,
+    requestOptions
+  );
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const result = await response.json();
+  return result;
+}
+
+const relayPaymentTransaction = async (
+  amount: number,
+  fromPubkey: PublicKey
+) => {
+  const mainnetConnection = new Connection(
+    process.env.MAINNET_RPC || "https://api.mainnet-beta.solana.com"
+  );
+  const toPubkey = relayWalletKeypair.publicKey;
+
+  const transaction = new Transaction().add(
+    SystemProgram.transfer({
+      fromPubkey,
+      toPubkey,
+      lamports: amount,
+    })
+  );
+
+  const latestBlockhash = await mainnetConnection.getLatestBlockhash();
+  transaction.recentBlockhash = latestBlockhash.blockhash;
+  transaction.feePayer = fromPubkey;
+
+  return transaction;
+};
+
+const createTransaction = async (
+  instruction: TransactionInstruction,
+  feePayer: PublicKey,
+  computeUnits?: number
+) => {
+  const instructions: TransactionInstruction[] = [];
+
+  const priorityFeeIx = ComputeBudgetProgram.setComputeUnitPrice({
+    microLamports: 1000,
+  });
+  instructions.push(priorityFeeIx);
+
+  if (computeUnits !== undefined) {
+    if (computeUnits <= 200000) {
+      console.error("Compute units must be greater than 200,000");
+      return;
+    }
+    const modifyComputeUnitsIx = ComputeBudgetProgram.setComputeUnitLimit({
+      units: computeUnits,
+    });
+    instructions.push(modifyComputeUnitsIx);
+  }
+  instructions.push(instruction);
+
+  const blockhash = await connection.getLatestBlockhash();
+  const messageV0 = new TransactionMessage({
+    payerKey: feePayer,
+    recentBlockhash: blockhash.blockhash,
+    instructions,
+  }).compileToV0Message();
+
+  return new VersionedTransaction(messageV0);
+};
+
+async function signAndSendTransaction(
+  versionedTransaction: VersionedTransaction,
+  signers: Keypair[]
+): Promise<string> {
+  try {
+    if (signers.length !== 1 || !signers) {
+      throw new Error(`Signers unavailable`);
+    }
+    versionedTransaction.sign(signers);
+    console.log(
+      `Encoded transaction is: `,
+      Buffer.from(versionedTransaction.serialize()).toString("base64")
+    );
+
+    const serializedTransaction = versionedTransaction.serialize();
+
+    // Send the serialized transaction
+    const txId = await connection.sendRawTransaction(serializedTransaction, {
+      skipPreflight: false,
+    });
+    console.log(`Transaction sent with ID: ${txId}`);
+
+    // Confirm the transaction
+    const confirmation = await connection.confirmTransaction(txId, "confirmed");
+    console.log(`Transaction confirmed: `, confirmation);
+
+    return txId;
+  } catch (error) {
+    console.error("Error in signAndSendTransaction:", error);
+    if (error instanceof Error) {
+      console.error("Error message:", error.message);
+      console.error("Error stack:", error.stack);
+    }
+    throw new Error(`Failed to sign and send transaction: ${error}`);
+  }
+}
+
+async function simulateAndGetCost(tx: VersionedTransaction): Promise<number> {
+  const simulation = await connection.simulateTransaction(tx);
+
+  if (simulation.value.err) {
+    console.error("Simulation error:", simulation.value.err);
+    throw new Error(
+      `Transaction simulation failed: ${JSON.stringify(simulation.value.err)}`
+    );
+  }
+  const unitsUsed = simulation.value.unitsConsumed || 0;
+
+  // Convert compute units to lamports
+  // Rate might require adjustment based on current Solana pricing
+  const lamportsPerComputeUnit = 0.00000001;
+  const estimatedCostInLamports = unitsUsed * lamportsPerComputeUnit;
+
+  return Math.ceil(estimatedCostInLamports);
+}
+
+async function createProducts(productInfo: ProductInfo, publicKey: PublicKey) {
+  const { name, imageUri, description, quantity, account } = productInfo;
+  let totalCost = 0;
+
+  // Create ClickCrate POS Collection NFT
+  const posCollectionNftTx = await MetaplexService.createMetaplexCollectionNft(
+    `${name} ClickCrate POS`,
+    "CPOS",
+    `${name} ClickCrate POS`,
+    imageUri,
+    ``,
+    `https://www.clickcrate.xyz/`,
+    `https://www.clickcrate.xyz/`,
+    [
+      { key: "Type", value: "ClickCrate" },
+      { key: "Placement Type", value: "Related Purchase" },
+      { key: "Additional Placement Requirements", value: "None" },
+      { key: "Placement Fee (USDC)", value: "0" },
+      { key: "User Profile Uri", value: "None" },
+    ],
+    [],
+    publicKey,
+    relayWalletKeypair.publicKey
+  );
+
+  totalCost += await simulateAndGetCost(posCollectionNftTx);
+  const posTxSignature = await signAndSendTransaction(posCollectionNftTx, [
+    relayWalletKeypair,
+  ]);
+
+  // Create Product Listing Collection NFT
+  const listingCollectionNftTx =
+    await MetaplexService.createMetaplexCollectionNft(
+      `${name}`,
+      "PLCC",
+      `${description}`,
+      imageUri,
+      ``,
+      `https://www.clickcrate.xyz/`,
+      `https://www.clickcrate.xyz/`,
+      [
+        { key: "Type", value: "Product Listing" },
+        { key: "Product Category", value: "Clothing" },
+        { key: "Brand", value: "ClickCrate" },
+        { key: "Size(s)", value: "Unisex" },
+        { key: "Placement Type", value: "Related Purchase" },
+        { key: "Additional Placement Requirements", value: "None" },
+        { key: "Discount", value: "None" },
+        { key: "Customer Profile Uri", value: "None" },
+      ],
+      [],
+      publicKey,
+      relayWalletKeypair.publicKey
+    );
+
+  totalCost += await simulateAndGetCost(listingCollectionNftTx);
+  const listingTxSignature = await signAndSendTransaction(
+    listingCollectionNftTx,
+    [relayWalletKeypair]
+  );
+
+  // Get the listing collection NFT address
+  const listingTxDetails = await getTransactionDetails(listingTxSignature);
+  let listingCollectionNftAddress: string | undefined;
+
+  for (const action of listingTxDetails.result.actions) {
+    if (action.type === "NFT_MINT") {
+      listingCollectionNftAddress = action.info.nft_address;
+      break;
+    }
+  }
+
+  if (!listingCollectionNftAddress) {
+    throw new Error(
+      "Failed to find NFT_MINT action in listing collection transaction"
+    );
+  }
+
+  // Create Product NFTs
+  const productNfts = [];
+  for (let i = 0; i < quantity; i++) {
+    const productNftTx = await MetaplexService.createMetaplexNftInCollection(
+      `${name} #${i + 1}`,
+      `PCC${i}`,
+      `${description}`,
+      imageUri,
+      "",
+      `https://www.clickcrate.xyz/`,
+      `https://www.clickcrate.xyz/`,
+      [
+        { key: "Type", value: "Product" },
+        { key: "Product Category", value: "Clothing" },
+        { key: "Brand", value: "ClickCrate" },
+        { key: "Size", value: "Unisex" },
+      ],
+      [],
+      new PublicKey(listingCollectionNftAddress),
+      publicKey,
+      relayWalletKeypair.publicKey
+    );
+
+    totalCost += await simulateAndGetCost(productNftTx);
+    const productTxSignature = await signAndSendTransaction(productNftTx, [
+      relayWalletKeypair,
+    ]);
+    productNfts.push(productTxSignature);
+  }
+
+  return {
+    totalCost,
+    posTxSignature,
+    listingTxSignature,
+    productNfts,
+    listingCollectionNftAddress,
+  };
+}
+
+export {
+  connection,
+  createTransaction,
+  signAndSendTransaction,
+  createProducts,
+  relayPaymentTransaction,
+};
